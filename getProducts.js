9a94df9e4bc444008f35a595b8bbc3a5069ed985
NansiKisil <nansi21@ukr.net>
2022-04-07 21:19:16 +0300

    add products price

--- getProducts.js
+++ getProducts.js
@@ -1,3 +1,14 @@
 const products = require('./products.json');
 
 exports.getProducts = () => products;
+console.log("Продукт\t\t\t\t| Цена");
+
+products.forEach(element => {
+    console.log(
+        `${element.product}\t\t|${element.prices}`
+      );
+});
+
+// for(let counter = 0; counter < products.length; counter++){
+//     console.log(products[counter].product, products[counter].prices);
+// }
